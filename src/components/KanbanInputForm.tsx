c550e1c91598fe13cf1385ba6c81c746a4082eb7
Droggelbecher92 <thomaskittlaus@gmail.com>
2022-06-16 16:56:18 +0200

    added post new Kanban

--- src/components/KanbanInputForm.tsx
+++ src/components/KanbanInputForm.tsx
@@ -1,11 +1,35 @@
 import './KanbanInputForm.css'
+import {FormEvent, useEffect, useState} from "react";
+import {KanbanStatus} from "../service/models";
+import {postNewKanban} from "../service/apiService";
 
 export default function KanbanInputForm(){
+
+    const [task, setTask] = useState(localStorage.getItem('task')??'')
+    const [desc, setDesc] = useState(localStorage.getItem('desc')??'')
+
+    useEffect(()=>{
+        localStorage.setItem("task",task)
+    },[task])
+
+    useEffect(()=>{
+        localStorage.setItem("desc",desc)
+    },[desc])
+
+    const saveNewKanban = (ev : FormEvent) =>  {
+        ev.preventDefault()
+        postNewKanban({'task':task,'description':desc,'status':KanbanStatus.OPEN})
+            .then(()=>{
+                setDesc('')
+                setTask('')
+            })
+    }
+
     return(
-        <div className={'kanbanInputForm'}>
-            <input className={'inputForm'} type="text"/>
-            <input className={'inputForm'} type="text"/>
-            <button>Save</button>
-        </div>
+        <form className={'kanbanInputForm'} onSubmit={saveNewKanban}>
+            <input className={'inputForm'} type="text" placeholder={'Neuer Task'} value={task} onChange={ev => setTask(ev.target.value)}/>
+            <input className={'inputForm'} type="text" placeholder={'Beschreibung'} value={desc} onChange={ev => setDesc(ev.target.value)}/>
+            <button type={'submit'}>Save</button>
+        </form>
     )
 }
