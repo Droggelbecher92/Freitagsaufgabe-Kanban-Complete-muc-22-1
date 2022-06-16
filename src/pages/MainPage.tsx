80b74be773ee1a88997e00dfe695fca69f75e47e
Droggelbecher92 <thomaskittlaus@gmail.com>
2022-06-16 15:44:13 +0200

    build basic template

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,67 @@
+export default function MainPage(){
+    return(
+        <div className={'mainPage'}>
+            <div>
+                <h1>Kanban Dudette</h1>
+            </div>
+            <div>
+                <input type="text"/>
+                <input type="text"/>
+                <button>Save</button>
+            </div>
+            <div>
+                <div>
+                    <h3>Status 1</h3>
+                    <div>
+                        <p>Task</p>
+                        <p>Beschreibung</p>
+                        <button>???</button>
+                        <button>???</button>
+                        <button>???</button>
+                    </div>
+                    <div>
+                        <p>Task</p>
+                        <p>Beschreibung</p>
+                        <button>???</button>
+                        <button>???</button>
+                        <button>???</button>
+                    </div>
+                </div>
+                <div>
+                    <h3>Status 1</h3>
+                    <div>
+                        <p>Task</p>
+                        <p>Beschreibung</p>
+                        <button>???</button>
+                        <button>???</button>
+                        <button>???</button>
+                    </div>
+                    <div>
+                        <p>Task</p>
+                        <p>Beschreibung</p>
+                        <button>???</button>
+                        <button>???</button>
+                        <button>???</button>
+                    </div>
+                </div>
+                <div>
+                    <h3>Status 1</h3>
+                    <div>
+                        <p>Task</p>
+                        <p>Beschreibung</p>
+                        <button>???</button>
+                        <button>???</button>
+                        <button>???</button>
+                    </div>
+                    <div>
+                        <p>Task</p>
+                        <p>Beschreibung</p>
+                        <button>???</button>
+                        <button>???</button>
+                        <button>???</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
